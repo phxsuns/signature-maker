5fba2bbccfb7c25d3d38540e5c5c1cfdce9d5d0d
phxsuns <sunxuhao@gmail.com>
2014-02-09 23:27:45 +0800

    新部门邮件签名

--- assets/sm.js
+++ assets/sm.js
@@ -1,148 +1,45 @@
 (function($){
 
-	//var fontZhonghei = new Font('assets/zhonghei.ttf','FZZH');
-	var fontZhunhei = new Font('assets/zhunhei.ttf','FZZHH');
-	var painterSM = new Painter('sm',{width:670,height:150});
+	var fontName = 'Microsoft Yahei';
+	var painterSM = new Painter('sm',{width:331,height:291});
 
 	var make = function(info){
 		
 		painterSM.init();
 		//背景
-		painterSM.add('Image',{src:'assets/bg-sign.png'},{x:0,y:0},0);
-		//头像
-		painterSM.add('Image',{image:info.image,width:80,height:80},{x:206,y:23},1);
-		painterSM.add('Image',{src:'assets/face-mask.png',width:80,height:80},{x:206,y:23},2);//圆形遮罩
+		painterSM.add('Image',{src:'assets/bg.png'},{x:0,y:0},0);
 		//姓名
-		painterSM.add('Text',{text:info.rname,color:'#5E6D81',size:22,bold:'',fontObj:fontZhunhei},{x:310,y:32},1);
+		painterSM.add('Text',{text:info.realname,color:'#999999',size:26,bold:'',font:fontName},{x:129,y:114},1);
 		
-		var rnameWidth = 23 * info.rname.length;
-		painterSM.add('Text',{text:info.fname,color:'#5E6D81',size:16,bold:'',fontObj:fontZhunhei},{x:310 + rnameWidth,y:38},1);
+		var realnameWidth = 27 * info.realname.length;
+		painterSM.add('Text',{text:info.nickname,color:'#999999',size:18,bold:'',font:fontName},{x:129 + realnameWidth,y:122},1);
 		
 		//部门
-		painterSM.add('Text',{text:info.department,color:'#9EA7B3',size:13,fontObj:fontZhunhei},{x:310,y:62},1);
+		painterSM.add('Text',{text:info.department,color:'#bdbdbd',size:12,font:fontName},{x:129,y:150},1);
 		
-		//地址
-		painterSM.add('Text',{text:info.address,color:'#888888',size:11,fontObj:fontZhunhei},{x:310,y:89},1);
+		//职位
+		painterSM.add('Text',{text:info.title,color:'#bdbdbd',size:12,font:fontName},{x:129,y:168},1);
 		
+		//座位
+		painterSM.add('Text',{text:info.position,color:'#9d9d9d',size:12,font:fontName},{x:150,y:192},1);
+
 		//手机
-		painterSM.add('Text',{text:info.phone,color:'#888888',size:11,fontObj:fontZhunhei},{x:310,y:108},1);
+		painterSM.add('Text',{text:info.mobile,color:'#9d9d9d',size:12,font:fontName},{x:46,y:250},1);
 		
 		//座机
-		painterSM.add('Text',{text:info.telphone,color:'#888888',size:11,fontObj:fontZhunhei},{x:435,y:108},1);
+		painterSM.add('Text',{text:info.telphone,color:'#9d9d9d',size:12,font:fontName},{x:198,y:250},1);
 
-		//信息列表
-		/* var listY = [36,52,68,84,100];
-		var listColor = '#888888';
-		var listText = [info.department,info.telphone,info.phone,info.address,info.site];
-
-		var n = 0;
-		for(var i = 0;i < listText.length;i++){
-			if(listText[i]){
-				painterSM.add('Text',{text:listText[i],color:listColor,fontObj:fontZhunhei,size:12},{x:350,y:listY[n]},1);
-				n++;
-			}
-		} */
-		// painterSM.add('Text',{text:info.department,color:'#888888'},{x:350,y:36},1);
-		// painterSM.add('Text',{text:info.telphone,color:'#888888'},{x:350,y:52},1);
-		// painterSM.add('Text',{text:info.phone,color:'#888888'},{x:350,y:68},1);
-		// painterSM.add('Text',{text:'地址：杭州市西湖区华星路96号瑞利大厦6楼',color:'#888888'},{x:350,y:84},1);
-		// painterSM.add('Text',{text:'网址：daxue.taobao.com',color:'#888888'},{x:350,y:100},1);
 		painterSM.draw();
 		
-	};
-	
-	var croper = {
-		data: {},
-		init: function(){
-			var me = this;
-			//裁剪插件
-			var ias = $('#imgPhoto').imgAreaSelect({
-				handles: true,
-				instance: true,
-				aspectRatio: '1:1',
-				fadeSpeed: 100,
-				minHeight: 80,
-				minWidth: 80,
-				x1: 0,
-				y1: 0,
-				x2: 80,
-				y2: 80,
-				hide: true,
-				onSelectEnd: function(o,d){
-					//console.log(d);
-					me.data.result = d;
-				}
-			});
-			this.data.ias = ias;
-			this.data.result = {'x1':0,'y1':0,'x2':80,'y2':80,'width':80,'height':80};
-
-			//弹出框插件
-			var pop = $('#pop');
-			var mask = $('#mask');
-			pop.find('.h-close').bind('click',function(e){
-				e.preventDefault();
-				mask.fadeOut();
-				pop.fadeOut();
-				ias.setOptions({hide:1});
-			});
-
-			$('#imgSelectOk').bind('click',function(e){
-				e.preventDefault();
-				var result = me.data.result;
-				me.crop(result.x1,result.y1,result.width,result.height,function(data){
-					mask.fadeOut();
-					pop.fadeOut();
-					if(data) $('#imgFace')[0].src = data;
-					ias.setOptions({hide:1});
-				});
-				
-			});
-
-			return ias;
-		},
-		show: function(img){
-			var ias = this.data.ias;
-			if(!ias) ias = this.init();//第一次显示作初始化
-			var pop = $('#pop');
-			var mask = $('#mask');
-			pop.fadeIn();
-			mask.fadeIn();
-			var photo = $('#imgPhoto')[0];
-			photo.src = img.src;
-			photo.onload = function(){
-				ias.setOptions({show:1,imageHeight:img.height,imageWidth:img.width});
-			}
-			this.data.img = img;
-		},
-		crop: function(x,y,w,h,cb){
-			//裁剪
-			var img = this.data.img;
-			if(!img){
-				cb && cb('');
-			}
-			// var c = document.createElement('canvas');
-			// var ctx = c.getContext('2d');
-			// c.width = 60;
-			// c.height = 60;
-			// ctx.drawImage(img,x,y,w,h,0,0,60,60);	
-			// cb && cb(c.toDataURL());
-			var data = img.src;
-			var num = data.indexOf('base64,');
-			var code = data.substring(num+7);
-			$.post('cropper.php',{code:code,dw:80,dh:80,x:x,y:y,w:w,h:h},function(data){
-				cb && cb('data:image/png;base64,'+data.code);
-			},'json');
-		}
 	};
 
 	var tips = {
 		show: function(node,text){
-			node.find('.control-group').addClass('error');
-			node.find('.help-inline').text(text).addClass('show-inline');
+			node.parents('.form-group').addClass('has-error');
 		},
 		init: function(){
-			$('.control-group').on('mousedown',function(){
-				$('.control-group').removeClass('error').find('.help-inline').removeClass('show-inline');
+			$('.form').on('mousedown',function(){
+				$('.form-group').removeClass('has-error');
 			});
 		}
 	}
@@ -151,82 +48,79 @@
 
 		painterSM.render($('#smbox')[0]);
 
-
 		painterSM.on('drawComplete',function(){
 			$('#outbox').html('<img src="'+painterSM.canvas.toDataURL()+'">');
 			$('#down').show();
 		});
-		
 
 		$('#make').click(function(){
 
 			var isOk = true;
 
-			//获取头像
-			var imgAvatar = new Image();
-			imgAvatar.src = $('#imgFace')[0].src;
-
 			//获取名字
-			var rname = $('#rname').val();
-			var fname = $('#fname').val();
-			//var name = rname;
-			//name += fname ? '（'+fname+'）' : '';
-			if(!rname){
-				tips.show($('.step1'),'真名一定要填写哦～');
+			var realname = $('#realname').val();
+			var nickname = $('#nickname').val();
+
+			if(!realname){
+				tips.show($('#realname'));
 				isOk = false;
 			}
-			if(fname){
-				fname = '（'+fname+'）';
+			if(nickname){
+				nickname = '（' + nickname + '）';
 			}
 
 			//获取部门
 			var department = $('#department').val();
+			if(!department || department.substr(-1,1) === '-'){
+				tips.show($('#department'));
+				isOk = false;
+			}
 
-			//获取固定电话
-			var tel = $('#tel').val();
-			var telsub = $('#telsub').val();
-			var telphone = '电话：'+tel;
-			telphone += telsub ? '('+telsub+')' : '';
-			if(!tel) telphone = '';//若未填写，则不显示
-			else if(!tel.match(/^([0-9]|-)+$/) || (telsub && !telsub.match(/^[0-9]+$/))){
-				tips.show($('.step4'),'电话号码格式不正确');
+			//获取职位
+			var title = $('#title').val();
+			if(!title){
+				tips.show($('#title'));
+				isOk = false;
+			}
+
+			//获取座位
+			var position = $('#position').val();
+			if(!position || position.substr(-1,1) === '-'){
+				tips.show($('#position'));
 				isOk = false;
 			}
 
 			//获取手机
-			var tmpPhone = $('#phone').val();
-			var phone = '手机：'+ tmpPhone;
-			if(!tmpPhone){
-				tips.show($('.step5'),'手机号一定要填写哦～');
-				isOk = false;
-			}else if(tmpPhone.length != 11 || !tmpPhone.match(/^[0-9]+$/)){
-				tips.show($('.step5'),'手机号码格式不正确');
+			var mobile = $('#mobile').val();
+			if(!mobile || mobile.length != 11 || !mobile.match(/^[0-9]+$/)){
+				tips.show($('#mobile'));
 				isOk = false;
 			}
 
-			//设置地址
-			var address = '地址：杭州市文一西路969号阿里巴巴西溪园区6号楼3楼';
-
-			//设置网站
-			var site = '网站：http://daxue.taobao.com'
+			//获取固定电话
+			var telphone = $('#telphone').val();
+			if(!telphone.match(/^([0-9]|-)+$/) || telphone.length != 13){
+				tips.show($('#telphone'));
+				isOk = false;
+			}
 
 			//绘制
 			//isOk = 1;//调试代码
 			if(isOk){
 				make({
-					image:imgAvatar,
-					rname:rname,
-					fname:fname,
+					realname:realname,
+					nickname:nickname,
 					department:department,
-					telphone:telphone,
-					phone:phone,
-					address:address,
-					site:site
+					title:title,
+					position:position,
+					mobile:mobile,
+					telphone:telphone
 				});
-				$('#outbox').html('<div class="contentBar"><div id="block_1" class="barlittle"></div><div id="block_2" class="barlittle"></div><div id="block_3" class="barlittle"></div><div id="block_4" class="barlittle"></div><div id="block_5" class="barlittle"></div></div>');
+				$('#outbox').html('<img src="http://img01.taobaocdn.com/tps/i1/T1cKm3XkRpXXXXXXXX-48-48.gif">');
 			}
 
 		});
+
 		tips.init();//消除提示事件绑定
 
 		$('#down').click(function(){
@@ -236,25 +130,6 @@
 			var form = $('#fileDown');
 			form.find('input[name=code]').val(code);
 			form.submit();
-		});
-
-		$('#selectFace').click(function(){
-			$('#fileFace').click();
-		});
-
-		$('#fileFace').bind('change',function(e){
-			var file = this.files[0];
-			var reader = new FileReader();
-			reader.readAsDataURL(file);
-			reader.onload = function(e){
-				//$('#imgFace')[0].src = this.result;
-				var img = new Image();
-				img.src = this.result;
-				img.onload = function(){
-					croper.show(img);
-				}
-			}
-			$('#fileForm')[0].reset();
 		});
 		
 	});
--- index.php
+++ index.php
@@ -2,120 +2,58 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>淘大邮件签名工具</title>
+		<title>邮件签名工具</title>
 		<link type="text/css" rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-		<link type="text/css" rel="stylesheet" href="assets/imageareaselect/css/imgareaselect-animated.css" />
 		<link type="text/css" rel="stylesheet" href="assets/sm.css">
-		<link type="text/css" rel="stylesheet" href="assets/loading.css">
 		<script src="assets/jquery.min.js"></script>
-		<script src="assets/imageareaselect/js/jquery.imgareaselect.pack.js"></script>
-		<script src="assets/font.js"></script>
 		<script src="assets/painter.min.js"></script>
 		<script src="assets/sm.js"></script>
 	</head>
 	<body>
-		<div class="container" id="makerbox">
+		<div class="wrap">
 			<h2>邮件签名生成工具</h2>
-			<div class="row step1">
-				<div class="span2">
-					Step1.您的名字
+			<div class="content clearfix">
+				<div class="info">
+					<form class="form">
+						<div class="form-group">
+							<label class="control-label" for="realname">真名</label>
+							<input type="text" class="form-control" id="realname">
+						</div>
+						<div class="form-group">
+							<label class="control-label" for="nickname">花名</label>
+							<input type="text" class="form-control" id="nickname">
+						</div>
+						<div class="form-group">
+							<label class="control-label" for="department">部门</label>
+							<input type="text" class="form-control" id="department" value="数字阅读事业部-">
+						</div>
+						<div class="form-group">
+							<label class="control-label" for="title">职位</label>
+							<input type="text" class="form-control" id="title">
+						</div>
+						<div class="form-group">
+							<label class="control-label" for="position">座位</label>
+							<input type="text" class="form-control" id="position" value="西溪园区 3-5-S-">
+						</div>
+						<div class="form-group">
+							<label class="control-label" for="mobile">手机号</label>
+							<input type="text" class="form-control" id="mobile">
+						</div>
+						<div class="form-group">
+							<label class="control-label" for="telphone">座机号</label>
+							<input type="text" class="form-control" id="telphone" value="0571-811">
+						</div>
+						<button type="button" class="btn btn-primary" id="make">生　成</button>
+					</form>
 				</div>
-				<div class="span10 control-group">		
-					<input type="text" placeholder="真名" id="rname">
-					<input type="text" placeholder="花名" id="fname">
-					<span class="help-inline"></span>
-				</div>
-			</div>
-			<div class="row step2">
-				<div class="span2">
-					Step2.设置照片
-				</div>
-				<div class="span10 control-group">		
-					<img src="assets/face-default.png" id="imgFace">
-					<button class="btn" type="button" id="selectFace">选择照片</button>
-				</div>
-			</div>
-			<div class="row step3">
-				<div class="span2">
-					Step3.选择部门
-				</div>
-				<div class="span10 control-group">		
-					<select id="department">
-						<option>淘宝大学</option>
-						<option selected>淘宝大学-产品部</option>
-						<option>淘宝大学-大客户部</option>
-						<option>淘宝大学-渠道部</option>
-						<option>淘宝大学-校园部</option>
-						<option>淘宝大学-运营部</option>
-						<option>淘宝大学-运营部-学员关系</option>
-						<option>淘宝大学-运营部-活动营销</option>
-						<option>淘宝大学-在线培训中心</option>
-						<option>淘宝大学-在线培训中心-点播</option>
-						<option>淘宝大学-在线培训中心-直播</option>
-						<option>淘宝大学-在线培训中心-淘工作</option>
-						<option>淘宝大学-在线培训中心-网站产品</option>
-						<option>淘宝大学-在线培训中心-用户体验</option>
-					</select>
-					<span class="help-inline"></span>
-				</div>
-			</div>
-			<div class="row step4">
-				<div class="span2">
-					Step4.填写座机
-				</div>
-				<div class="span10 control-group">		
-					<input type="text" placeholder="座机号码（可不填）" id="tel" value="0571-85022088">
-					<input type="text" placeholder="分机号码" id="telsub">
-					<span class="help-inline"></span>
-				</div>
-			</div>
-			<div class="row step5">
-				<div class="span2">
-					Step5.填写手机
-				</div>
-				<div class="span10 control-group">		
-					<input type="text" placeholder="手机号码" id="phone">
-					<span class="help-inline"></span>
-				</div>
-			</div>
-			<div class="row">
-				<div class="span12">
-					<button class="btn btn-primary btn-large" type="button" id="make">生成签名</button>
-				</div>
-			</div>
-			<div class="row">
-				<div class="span12">
+				<div class="out">
 					<div id="outbox"></div>
-				</div>
-			</div>
-			<div class="row">
-				<div class="span12">
-					<button class="btn btn-info btn-large" type="button" id="down" style="display:none;">下载签名</button>
+					<button type="button" class="btn btn-success" id="down">下载签名</button>
 				</div>
 			</div>
 		</div>
-		
-		<div id="mask" class="mask"></div>
-		<div id="pop" class="pop modal">
-			<div class="modal-header">
-				<button type="button" class="close h-close" data-dismiss="modal" aria-hidden="true">×</button>
-				<h3>选取照片区域</h3>
-			</div>
-			<div class="modal-body">
-				<div class="photobox">
-					<div class="photobg">
-						<img src="" id="imgPhoto">
-					</div>
-				</div>
-			</div>
-			<div class="modal-footer">
-				<a href="#" class="btn btn-primary" id="imgSelectOk">确定</a>
-				<a href="#" class="btn h-close">取消</a>
-			</div>
-		</div>
-		
+
 		<div id="smbox" style="display:none;"></div>
-		<form id="fileForm" style="display:none"><input type="file" id="fileFace" style="visibility: hidden;" accept="image/*"></form>
 		<form id="fileDown" action="download.php" method="post" style="display:none"><input type="hidden" name="code"><input type="hidden" name="name" value="邮件签名图"></form>
 	</body>
 </html>
